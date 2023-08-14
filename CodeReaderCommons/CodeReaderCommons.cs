e6029e14b89ca859172601da82f5ce2c9e52d038
vestr <petera-david@seznam.cz>
2023-08-14 16:02:09 +0200

    Improve binarization a bit

--- CodeReaderCommons/CodeReaderCommons.cs
+++ CodeReaderCommons/CodeReaderCommons.cs
@@ -4,6 +4,15 @@ using SixLabors.ImageSharp;
 namespace CodeReaderCommons {
     public static class Commons {
 
+        private class WelfordSampleInfo {
+            public WelfordSampleInfo() {}
+            public int count = 0;
+            public double sampleMean = 0;
+            //M2 from Welford's online algorithm. When divided by (count - 1) the square root equals sampleDeviation.
+            //https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm
+            public double sampleM2 = 0;
+        }
+
         /// <summary>
         /// Binarizes grayscale input image based on a local tresholding algorithm.
         ///  
@@ -15,90 +24,108 @@ namespace CodeReaderCommons {
         public static Image<Rgba32> Binarize(Image<Rgba32> image) {
             
             //Possibly should be input parameter 
-            const int windowSize = 31;
+            const int windowSize = 25;
             //2D array of the running sum of pixel intensity values
-            int[,] intensitySumMatrix = new int[image.Width + ((windowSize / 2) * 2), image.Height + ((windowSize / 2) * 2)];
+            WelfordSampleInfo[,] deviationSampleMatrix = new WelfordSampleInfo[image.Width + ((windowSize / 2) * 2), image.Height + ((windowSize / 2) * 2)];
+            //TODO should be a function
+            for (int j = 0; j < image.Height + ((windowSize / 2) * 2); j++) {
+                for (int i = 0; i < image.Width + ((windowSize / 2) * 2); i++) {
+                    deviationSampleMatrix[i, j] = new WelfordSampleInfo();
+                }
+            }
+        
+            //2D array of the running sum of pixel intensity values
+            //int[,] intensitySumMatrix = new int[image.Width + ((windowSize / 2) * 2), image.Height + ((windowSize / 2) * 2)];
             //2D array of the running sum of pixel intensity deviation values
-            int[,] deviationSumMatrix = new int[image.Width + ((windowSize / 2) * 2), image.Height + ((windowSize / 2) * 2)];
+            //int[,] deviationSumMatrix = new int[image.Width + ((windowSize / 2) * 2), image.Height + ((windowSize / 2) * 2)];
             //the "+ 2" is there to avoid bound checks when convolving with kernel
             int[,] thresholdT1Matrix = new int[image.Width + 2, image.Height + 2];
-            int[,] thresholdT2Matrix = new int[image.Width, image.Height];
+            //int[,] thresholdT2Matrix = new int[image.Width, image.Height];
             int[,] convolutionKernel = new int[,] {{1, 1, 1}, {1, 2, 1}, {1, 1, 1}};
             const int kernelSize = 3;
             const int kernelValuesSum = 10;
             //Padded image to avoid bound checks
             Image<Rgba32> paddedImage = GetPaddedImage(image, windowSize / 2);
-
+            
             //First part of the algorithm. Calculate T1 from the paper.
             paddedImage.ProcessPixelRows(accessor => {
-                for (int y = windowSize / 2; y < accessor.Height - (windowSize / 2) - 1; y++) {
+                for (int y = windowSize / 2; y < accessor.Height - (windowSize / 2); y++) {
                     Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
 
                     // pixelRow.Length has the same value as accessor.Width,
                     // but using pixelRow.Length allows the JIT to optimize away bounds checks:
-                    for (int x = windowSize / 2; x < pixelRow.Length - (windowSize / 2) - 1; x++) {
+                    for (int x = windowSize / 2; x < pixelRow.Length - (windowSize / 2); x++) {
                         // Get a reference to the pixel at position x
                         ref Rgba32 pixel = ref pixelRow[x];
                         int pixelValue = (int)pixel.R;
 
-                        for (int j = y - (windowSize / 2); j < y + (windowSize / 2) - 1; j++) {
-                            for (int i = x - (windowSize / 2); i < x + (windowSize / 2) - 1; i++) {
+                        for (int j = y - (windowSize / 2); j <= y + (windowSize / 2); j++) {
+                            for (int i = x - (windowSize / 2); i <= x + (windowSize / 2); i++) {
                                 //Console.WriteLine($"{i}, {j},  matrix: {image.Width + (windowSize / 2)}x{image.Height + (windowSize / 2)}");
-                                intensitySumMatrix[i,j] += pixelValue;
+                                //intensitySumMatrix[i,j] += pixelValue;
+                                var deviationSampleInfo = deviationSampleMatrix[i, j];
+                                deviationSampleInfo.count++;
+                                double delta1 = pixelValue - deviationSampleInfo.sampleMean;
+                                deviationSampleInfo.sampleMean += (delta1 / deviationSampleInfo.count);
+                                double delta2 = pixelValue - deviationSampleInfo.sampleMean;
+                                deviationSampleInfo.sampleM2 += (delta1 * delta2);
                             }
                         }
                     }
                 }
             });
 
-            //Calculate the deviation (this and previous step unfortunatelly cannot be done in one iteration)
-            paddedImage.ProcessPixelRows(accessor => {
-                for (int y = windowSize / 2; y < accessor.Height - (windowSize / 2) - 1; y++) {
-                    Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
+            // //Calculate the deviation (this and previous step unfortunatelly cannot be done in one iteration)
+            // paddedImage.ProcessPixelRows(accessor => {
+            //     for (int y = windowSize / 2; y < accessor.Height - (windowSize / 2) - 1; y++) {
+            //         Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
 
-                    // pixelRow.Length has the same value as accessor.Width,
-                    // but using pixelRow.Length allows the JIT to optimize away bounds checks:
-                    for (int x = windowSize / 2; x < pixelRow.Length - (windowSize / 2) - 1; x++) {
-                        // Get a reference to the pixel at position x
-                        ref Rgba32 pixel = ref pixelRow[x];
-                        int pixelValue = (int)pixel.R;
+            //         // pixelRow.Length has the same value as accessor.Width,
+            //         // but using pixelRow.Length allows the JIT to optimize away bounds checks:
+            //         for (int x = windowSize / 2; x < pixelRow.Length - (windowSize / 2) - 1; x++) {
+            //             // Get a reference to the pixel at position x
+            //             ref Rgba32 pixel = ref pixelRow[x];
+            //             int pixelValue = (int)pixel.R;
 
-                        for (int j = y - (windowSize / 2); j < y + (windowSize / 2) - 1; j++) {
-                            for (int i = x - (windowSize / 2); i < x + (windowSize / 2) - 1; i++) {
-                                int windowMean = intensitySumMatrix[x - (windowSize / 2), y - (windowSize / 2)] / (windowSize * windowSize);
-                                int pixelDeviation = pixelValue - windowMean;
-                                deviationSumMatrix[i,j] += pixelDeviation * pixelDeviation;
-                            }
-                        }
-                    }
-                }
-            });
+            //             for (int j = y - (windowSize / 2); j < y + (windowSize / 2) - 1; j++) {
+            //                 for (int i = x - (windowSize / 2); i < x + (windowSize / 2) - 1; i++) {
+            //                     int windowMean = intensitySumMatrix[x - (windowSize / 2), y - (windowSize / 2)] / (windowSize * windowSize);
+            //                     int pixelDeviation = pixelValue - windowMean;
+            //                     deviationSumMatrix[i,j] += pixelDeviation * pixelDeviation;
+            //                 }
+            //             }
+            //         }
+            //     }
+            // });
+
+            // //Compute threshold
+            // for (int j = 0; j < image.Height; j++) {
+            //     for (int i = 0; i < image.Width; i++) {
+            //         int windowSizeSquared = windowSize * windowSize;
+            //         int windowMean = intensitySumMatrix[i + (windowSize / 2), j + (windowSize / 2)] / windowSizeSquared;
+            //         double windowStandardDeviation = Math.Sqrt((double)(deviationSumMatrix[i + (windowSize / 2), j + (windowSize / 2)] / (windowSizeSquared - 1)));
+            //         const int R = 1250;
+            //         int thresholdT1 = (int)(windowMean * (1 + (windowStandardDeviation / R)));
+            //         //Console.WriteLine($"{i}, {j},  matrix: {image.Width}x{image.Height}");
+            //         //"+ 1" to compensate for padding
+            //         thresholdT1Matrix[i + 1,j + 1] = thresholdT1;
+            //     }
+            // }
 
             //Compute threshold
             for (int j = 0; j < image.Height; j++) {
                 for (int i = 0; i < image.Width; i++) {
-                    int windowSizeSquared = windowSize * windowSize;
-                    int windowMean = intensitySumMatrix[i + (windowSize / 2), j + (windowSize / 2)] / windowSizeSquared;
-                    double windowStandardDeviation = Math.Sqrt((double)(deviationSumMatrix[i + (windowSize / 2), j + (windowSize / 2)] / (windowSizeSquared - 1)));
+                    var welfordInfo = deviationSampleMatrix[i + (windowSize / 2), j + (windowSize / 2)];
+                    double windowMean = welfordInfo.sampleMean;
+                    //Console.WriteLine($"sample: {windowMean}, real: {(double)intensitySumMatrix[i + (windowSize / 2), j + (windowSize / 2)] / (windowSize * windowSize)}");
+                    double windowStandardDeviation = Math.Sqrt(welfordInfo.sampleM2 / (welfordInfo.count - 1));
+                    //double windowStandardDeviationNotSample = Math.Sqrt((double)(deviationSumMatrix[i + (windowSize / 2), j + (windowSize / 2)] / ((windowSize * windowSize) - 1)));
+                    //Console.WriteLine($"sample: {windowStandardDeviation}, real: {windowStandardDeviationNotSample}");
                     const int R = 1250;
                     int thresholdT1 = (int)(windowMean * (1 + (windowStandardDeviation / R)));
                     //Console.WriteLine($"{i}, {j},  matrix: {image.Width}x{image.Height}");
                     //"+ 1" to compensate for padding
                     thresholdT1Matrix[i + 1,j + 1] = thresholdT1;
-                }
-            }
-
-            //Convolve threshold matrix with kernel. Step 2 from the paper.
-            for (int j = 1; j < image.Height + 1; j++) {
-                for (int i = 1; i < image.Width + 1; i++) {
-                    int convolutionSum = 0;
-                    for (int l = -(kernelSize / 2); l < (kernelSize / 2) + 1; l++) {
-                        for (int k = -(kernelSize / 2); k < (kernelSize / 2) + 1; k++) {
-                            convolutionSum += convolutionKernel[(kernelSize / 2) + k, (kernelSize / 2) + l] * thresholdT1Matrix[i + k, j + l];
-                        }
-                    }
-
-                    thresholdT2Matrix[i - 1, j - 1] = convolutionSum / kernelValuesSum;
                 }
             }
 
@@ -110,11 +137,20 @@ namespace CodeReaderCommons {
                     // pixelRow.Length has the same value as accessor.Width,
                     // but using pixelRow.Length allows the JIT to optimize away bounds checks:
                     for (int x = 0; x < pixelRow.Length; x++) {
+                        //TODO: make function from convolution
+                        int convolutionSum = 0;
+                        for (int j = -(kernelSize / 2); j <= (kernelSize / 2); j++) {
+                            for (int i = -(kernelSize / 2); i <= (kernelSize / 2); i++) {
+                                convolutionSum += convolutionKernel[(kernelSize / 2) + i, (kernelSize / 2) + j] * thresholdT1Matrix[x + i + 1, y + j + 1];
+                            }
+                        }
+                        int thresholdT2 = convolutionSum / kernelValuesSum;
+
                         // Get a reference to the pixel at position x
                         ref Rgba32 pixel = ref pixelRow[x];
                         int pixelValue = pixel.R;
 
-                        if (pixelValue > thresholdT2Matrix[x, y]) {
+                        if (pixelValue >= thresholdT2) { //thresholdT2Matrix[x, y]
                             pixel = Color.White;
                         }   
                         else {
@@ -124,7 +160,7 @@ namespace CodeReaderCommons {
                 }
             });
 
-            return image;
+            return image; 
         }
         
 
--- OpenReader/Program.cs
+++ OpenReader/Program.cs
@@ -1,22 +1,26 @@
 ﻿using SixLabors.ImageSharp.Processing.Processors.Binarization;
 using CodeReaderCommons;
+using System.Diagnostics;
 
 namespace QRCodeReader {
     public class Program {
         public static void Main(string[] args) {
-            // Bitmap test = new Bitmap("../TestData/QRCodeTest1.jpeg");
-            // CodeReaderCommons.ProcessUsingLockbitsAndUnsafeAndParallel(test);
 
             using (Image<Rgba32> image = Image.Load<Rgba32>("../TestData/QRCodeTest1.jpeg")) {
-                
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
                 image.Mutate(x => x.Resize(300,0));
                 image.Mutate(x => x.Grayscale());
+                //image.Mutate(x => x.AdaptiveThreshold());
                 var binarizedImage = Commons.Binarize(image);
 
-                //ImageProcessor<Rgba32> binarizationProcessor = CreatePixelSpecificProcessor<Rgba32>(new Configuration(), test, new Rectangle(0, 0, test.Width, test.Height));
-                //binarizationProcessor.Execute();
-
+                // ImageProcessor<Rgba32> binarizationProcessor = CreatePixelSpecificProcessor<Rgba32>(new Configuration(), image, new Rectangle(0, 0, image.Width, image.Height));
+                // binarizationProcessor.Execute();
+                sw.Stop();
+                Console.WriteLine($"time: {sw.Elapsed}");
                 binarizedImage.Save("../TestData/QRCodeTest1OUTPUT.png");
+                
+                //image.Save("../TestData/QRCodeTest1OUTPUT.png");
             }
         }
     }
