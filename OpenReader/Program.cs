1fa0b7ccc720b00a5683029c4d90010a6fd8fb6b
vestr <petera-david@seznam.cz>
2023-09-01 20:59:07 +0200

    Better output to user

--- OpenReader/Program.cs
+++ OpenReader/Program.cs
@@ -8,12 +8,16 @@ namespace QRCodeReader {
         public static void Main(string[] args) {
             I2DCodeScanner scanner = new QRScanner();
             ScanResult result;
-            using (Image<Rgba32> image = Image.Load<Rgba32>("../TestData/QRCodeTest2.png")) {
+            using (Image<Rgba32> image = Image.Load<Rgba32>("../TestData/QRCodeTest1.png")) {
                 result = scanner.Scan(image);
             }
             
 
-            Console.WriteLine(result);
+            if (!result.Success) {
+                Console.WriteLine("QR code could not be decoded.");
+            }
+
+            Console.WriteLine($"result: {result.Data}");
         }
     }
 }
